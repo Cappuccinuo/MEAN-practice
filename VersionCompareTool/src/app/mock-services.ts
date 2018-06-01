05509d300993c01f30cb92219a27796ea4c45551
Yuan Gao <gaoyuanbj@yahoo.com>
2018-06-01 10:23:05 -0400

    Initial code + add a new component

---
+++ VersionCompareTool/src/app/mock-services.ts
@@ -0,0 +1,9 @@
+import {Service} from './service';
+
+export const SERVICES: Service[] = [
+  { id: 1, name: 'alert', environment: 'demo', version: '1.0.1' },
+  { id: 2, name: 'alertType', environment: 'ro', version: '1.0.2' },
+  { id: 3, name: 'publisher', environment: 'prd', version: '1.0.3' },
+  { id: 4, name: 'drillingapi', environment: 'ro', version: '1.0.4' },
+  { id: 5, name: 'well', environment: 'demo', version: '1.0.5' }
+]
