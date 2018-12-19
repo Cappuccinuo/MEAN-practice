1420bc9f95832ab5372a812231ce67bf1179b029
Yuan Gao <gaoyuanbj@yahoo.com>
2018-12-19 10:17:42 -0500

    Try angular material

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import {PostCreateComponent} from './posts/post-create/post-create.component'
+import {PostCreateComponent} from './posts/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,11 @@ import {PostCreateComponent} from './posts/post-create/post-create.component'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
