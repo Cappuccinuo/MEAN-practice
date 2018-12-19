a45f9c6c3dfb08141672ce3f780d2a5503df1cab
Yuan Gao <gaoyuanbj@yahoo.com>
2018-12-18 23:45:23 -0500

    twoWay Binding

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -6,8 +6,9 @@ import { Component } from '@angular/core';
 })
 export class PostCreateComponent {
   newPost = '';
+  enteredValue = '';
 
-  onAddPost(postInput: HTMLTextAreaElement) {
-    this.newPost = postInput.value;
+  onAddPost() {
+    this.newPost = this.enteredValue;
   }
 }
