4b6fb0b12b6390a7d6045a262e2a76c8acb8a819
Yuan Gao <gaoyuanbj@yahoo.com>
2018-12-20 12:00:19 -0500

    Fetching Initial Posts

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: "First",
+      title: "First post",
+      content: "Go up"
+    },
+    {
+      id: "Second",
+      title: "Second post",
+      content: "Go down"
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
