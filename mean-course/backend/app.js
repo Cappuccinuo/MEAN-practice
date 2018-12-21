dcab70c3f5698cadd617e7dd009cc4df7e8c7cb5
Yuan Gao <gaoyuanbj@yahoo.com>
2018-12-20 21:16:14 -0500

    Fetching Data From a Database

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -37,23 +37,14 @@ app.post('/api/posts', (req, res, next) => {
   });
 });
 
-app.use('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: "First",
-      title: "First post",
-      content: "Go up"
-    },
-    {
-      id: "Second",
-      title: "Second post",
-      content: "Go down"
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
-  });
+app.get('/api/posts', (req, res, next) => {
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+      });
+    });
 });
 
 module.exports = app;
