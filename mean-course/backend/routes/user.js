228fd5d0619018dd2ee019c8da58fa322c018329
Yuan Gao <gaoyuanbj@yahoo.com>
2019-01-14 20:09:05 -0500

    Creating a New User Upon Request

--- mean-course/backend/routes/user.js
+++ mean-course/backend/routes/user.js
@@ -1,7 +1,27 @@
 const express = require('express');
-const Post = require('../models/post');
+const User = require('../models/user');
 const router = express.Router();
-
-router.post('/signup');
+const bcrypt = require('bcrypt');
+router.post('/signup', (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+        .then(result => {
+          res.status(201).json({
+            message: 'User created!',
+            result: result
+          });
+        })
+        .catch(err => {
+          res.status(500).json({
+            error: err
+          });
+        });
+    });
+});
 
 module.exports = router;
